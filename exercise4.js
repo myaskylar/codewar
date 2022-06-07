4831e349ea553ece9e40b5b5c3df2cd252acb7ff
Mya <myaskylar14@gmail.com>
2022-06-07 20:28:33 +0100

    refactor

--- exercise4.js
+++ exercise4.js
@@ -15,26 +15,27 @@ const arrange = (arr) => {
     elem - arr[index - 1] != 1 && elem - arr[index + 1] != -1 ? emt.push(elem) : null;
     elem - arr[index - 1] != 1 && elem - arr[index + 1] == -1 ? emt.push(elem) : null;
     elem - arr[index - 1] == 1 && elem - arr[index + 1] == -1 ? emt.push("-") : null;
-    elem - arr[index + 1] != -1 ? emt.push(elem) : null;
  })
 
 let filterArr = emt
   .filter((elem, index) => {
     return elem != emt[index + 1];
   })
+  console.log(filterArr);
 
  return filterArr.reduce(
    (text, value, i, array) =>
-     text + (array[i - 1] == "-" || array[i + 1] != "-" && value != "-" ? value + "," : value)
+      text + (array[i - 1] == "-" || array[i + 1] != "-" && value != "-" ? value + "," :  value)
+     
  );
 
 }
 
-const solution = (list) =>{
-   return list.reduce((text, value, index, array)=>
-      text + "," + (value - array[index + 1] != -1 ? value : "no")
-    )
-}
+// const solution = (list) =>{
+//    return list.reduce((text, value, index, array)=>
+//       text + "," + (value - array[index + 1] != -1 ? value : "no")
+//     )
+// }
 // -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
 //"-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
 // -6, -3 - 1, 3 - 5, 7 - 11, 14, 15, 17 - 20;
@@ -42,4 +43,5 @@ const solution = (list) =>{
 //    (text, value, i, array) =>
 //      text + (i < array.length - 1 ? ", " : " or ") + value
 //  );
-console.log(solution(array2));
+console.log(arrange(array));
+console.log(arrange(array2));
