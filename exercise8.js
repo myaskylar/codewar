c610f7b87f170fc3eb3664c87ee0b2795d06f9c9
Mya <myaskylar14@gmail.com>
2022-06-12 11:49:45 +0100

    new kata practice

---
+++ exercise8.js
@@ -0,0 +1,21 @@
+Array.prototype.sameStructureAs = function (other) {
+  // Return 'true' if and only if 'other' has the same
+  // nesting structure as 'this'.
+  // Note: You are given a function isArray(o) that returns
+  // whether its argument is an array.
+};
+
+
+ // should return true
+[ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );          
+[ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] );  
+
+ // should return false 
+[ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );  
+[ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] );  
+
+// should return true
+[ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] ); 
+
+// should return false
+[ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );    
